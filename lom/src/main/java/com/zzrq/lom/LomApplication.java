869130ab97673eb368ecef5d034c341a3aeedad1
仙仙 <2261567142@qq.com>
2019-02-16 14:50:24 +0800

    lxx first

---
+++ lom/src/main/java/com/zzrq/lom/LomApplication.java
@@ -0,0 +1,14 @@
+package com.zzrq.lom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LomApplication.class, args);
+    }
+
+}
+
